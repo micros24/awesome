facfe0fe571c08c42af8ec5714343d80360d6447
micros24 <jasperjano.ph@protonmail.com>
2023-07-06 09:16:11 +0800

    Cleaned comments

--- awesome/awesome/rc.lua
+++ awesome/awesome/rc.lua
@@ -56,8 +56,8 @@ beautiful.init("/home/micro/.config/awesome/default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "alacritty"
-editor = os.getenv("EDITOR") or "nano"
-editor_cmd = terminal .. " -e " .. editor
+-- editor = os.getenv("EDITOR") or "nano"
+-- editor_cmd = terminal .. " -e " .. editor
 
 -- Default modkey.
 modkey = "Mod4"
@@ -88,25 +88,23 @@ awful.layout.layouts = {
 -- -- }}}
 
 -- {{{ Wibar
-local function set_wallpaper(s)
-    -- Wallpaper
-    if beautiful.wallpaper then
+--local function set_wallpaper(s)
+--    -- Wallpaper
+--    if beautiful.wallpaper then
         -- local wallpaper = beautiful.wallpaper
         -- If wallpaper is a function, call it with the screen
         -- if type(wallpaper) == "function" then
         --     wallpaper = wallpaper(s)
         -- end
         -- gears.wallpaper.maximized(wallpaper, s, false)
-        gears.wallpaper.fit(beautiful.wallpaper, s)
-    end
-end 
+--        gears.wallpaper.fit(beautiful.wallpaper, s)
+--    end
+--end 
 
 -- Re-set wallpaper when a screen's geometry changes (e.g. different resolution)
-screen.connect_signal("property::geometry", set_wallpaper)
+-- screen.connect_signal("property::geometry", set_wallpaper)
 
-for s in screen do
-    -- set_wallpaper(s)
-    
+for s in screen do    
     if s.index == 1 then
         awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, awful.layout.layouts[1])
 
@@ -115,26 +113,6 @@ for s in screen do
         awful.tag({ "A", "B", "C", "D", "E", "F", "G", "H", "I" }, s, awful.layout.layouts[1])
     end
 end
-
--- awful.screen.connect_for_each_screen(function(s)
---     -- Wallpaper
---     set_wallpaper(s)
--- 
---     -- Each screen has its own tag table.
---     awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, awful.layout.layouts[1])
--- 
---     -- Create a promptbox for each screen
---     -- s.mypromptbox = awful.widget.prompt()
--- end)
--- }}}
-
--- -- {{{ Mouse bindings
--- root.buttons(gears.table.join(
---     awful.button({ }, 3, function () mymainmenu:toggle() end),
---     awful.button({ }, 4, awful.tag.viewnext),
---     awful.button({ }, 5, awful.tag.viewprev)
--- ))
--- -- }}}
 
 -- {{{ Key bindings
 globalkeys = gears.table.join(
@@ -481,11 +459,11 @@ end)
 
 -- Enable sloppy focus, so that focus follows mouse.
 client.connect_signal("mouse::enter", function(c)
-    c:emit_signal("request::activate", "mouse_enter", {raise = false})
+    c:emit_signal("request::activate", "mouse_enter", {raise = true})
 end)
 
-client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
-client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
+--client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
+--client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 
          -- local paddingValue = { bottom = 18 }
 	 -- awful.screen.focused().padding = paddingValue;
